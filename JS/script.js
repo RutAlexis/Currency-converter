ad726e3d39a614ed21ddeff85a3a4a1615d3e89f
Aleksandra Rutkowska <adrabikm@gmail.com>
2023-01-13 10:51:44 +0100

    added arrow function

--- JS/script.js
+++ JS/script.js
@@ -2,45 +2,39 @@
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const amountElement = document.querySelector(".js-amount");
-
         const currencySelectionElement = document.querySelector(".js-currencySelection");
         const currency = currencySelectionElement.value;
 
         let currencyValue;
-
-        const amount = +amountElement.value;
-        const result = amount / currencyValue;
-        const resultElement = document.querySelector(".js-result");
-
-        resultElement.innerText = `${result.toFixed(2)} ${" "}  ${currency}`;
-    }
-
-    const init = () => {
-
-        const formElement = document.querySelector(".js-form");
-
-        const currencySelectionElement = document.querySelector(".js-currencySelection");
-        const currency = currencySelectionElement.value;
-
-        let currencyValue;
-        formElement.addEventListener("submit", onFormSubmit);
 
         switch (currency) {
-            case "EUR":
-                currencyValue = 4.70;
-                break;
 
             case "AUD":
                 currencyValue = 3.02;
+                break;
+
+            case "EUR":
+                currencyValue = 4.70;
                 break;
 
             case "NOK":
                 currencyValue = 0.44;
                 break;
         }
-    };
 
+        const resultElement = document.querySelector(".js-result");
+        const amountElement = document.querySelector(".js-amount");
+        const amount = amountElement.value;
+        const result = amount / currencyValue;
+
+        resultElement.innerText = `${result.toFixed(2)} ${currency} ${" "}`;
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    }
     init();
 }
 
