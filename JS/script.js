9c0783fa66102819341568885ebe62d6d76af8a0
Aleksandra Rutkowska <adrabikm@gmail.com>
2023-01-18 19:37:05 +0100

    added chooseCurrency and finishResult functions

--- JS/script.js
+++ JS/script.js
@@ -1,40 +1,42 @@
 {
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-
-        const currencySelectionElement = document.querySelector(".js-currencySelection");
-        const currency = currencySelectionElement.value;
-
-        let currencyValue;
+    const chooseCurrency = (currency) => {
 
         switch (currency) {
 
             case "AUD":
-                currencyValue = 3.02;
-                break;
+                return 3.02;
 
             case "EUR":
-                currencyValue = 4.70;
-                break;
+                return 4.70;
 
             case "NOK":
-                currencyValue = 0.44;
-                break;
-        }
+                return 0.44;
+        };
+    };
 
+    const finishResult = (result, currency) => {
         const resultElement = document.querySelector(".js-result");
-        const amountElement = document.querySelector(".js-amount");
-        const amount = amountElement.value;
-        const result = amount / currencyValue;
 
         resultElement.innerText = `${result.toFixed(2)} ${currency} ${" "}`;
-    }
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const currencySelectionElement = document.querySelector(".js-currencySelection");
+        const currency = currencySelectionElement.value;
+        const amountElement = document.querySelector(".js-amount");
+        const amount = amountElement.value;
+        const chooseCurrencyElement = chooseCurrency(currency);
+        const result = amount / chooseCurrencyElement;
+
+        finishResult(result, currency);
+    };
 
     const init = () => {
         const formElement = document.querySelector(".js-form");
 
         formElement.addEventListener("submit", onFormSubmit);
-    }
+    };
+
     init();
 }
-
